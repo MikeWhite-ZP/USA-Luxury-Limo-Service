ee022e7b8b66bfb72755fd23e25db2fe4a6bc5f7
MikeWhite-ZP <168873774+MikeWhite-ZP@users.noreply.github.com>
2025-11-17 07:46:04 -0600

    Create new_shema.sql

---
+++ database/new_shema.sql
@@ -0,0 +1,26 @@
+  GNU nano 7.2                                                postgres_schema.sql                                                          
+--
+-- PostgreSQL database dump
+--
+
+\restrict 52DQijBHXKMUbNUCzQXBTJstlSwvNYPSN3khZbYwfDxxhAfybnneFsY8uvnPBzy
+
+-- Dumped from database version 15.15
+-- Dumped by pg_dump version 15.15
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict 52DQijBHXKMUbNUCzQXBTJstlSwvNYPSN3khZbYwfDxxhAfybnneFsY8uvnPBzy
