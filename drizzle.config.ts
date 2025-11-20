bddfd55afdba6c917f88d6e4c2221f79de42c76f
MikeWhite-ZP <168873774+MikeWhite-ZP@users.noreply.github.com>
2025-11-20 19:42:41 +0000

    Fix drizzle-kit and merge remote changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
-export default defineConfig({
-  out: "./migrations",
-  schema: "./shared/schema.ts",
-  dialect: "postgresql",
+const config: Config = {
+  schema: './db/schema.ts',
+  out: './migrations',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
-});
+};
